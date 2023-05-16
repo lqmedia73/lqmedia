1c3c954392b8ab1d2a2940b8b4d8516d719ddefa
lqmedia73 <133774449+lqmedia73@users.noreply.github.com>
2023-05-16 22:26:22 +0900

    remove images

--- js/core/main.js
+++ js/core/main.js
@@ -41,78 +41,6 @@ const state = {
       slug: 'ThuongVo',
       fullName: 'Thương Vô'
     }),
-    Artist({
-      slug: 'NhuViet',
-      fullName: 'Như Việt'
-    }),
-    Artist({
-      slug: 'PhanDuyAnh',
-      fullName: 'Phan Duy Anh'
-    }),
-    Artist({
-      slug: 'MinhVuong',
-      fullName: 'Minh Vương'
-    }),
-    Artist({
-      slug: 'ThanhHung',
-      fullName: 'Thanh Hưng'
-    }),
-    Artist({
-      slug: 'ChauKhaiPhong',
-      fullName: 'Châu Khải Phong'
-    }),
-    Artist({
-      slug: 'KICM',
-      fullName: 'K-ICM'
-    }),
-    Artist({
-      slug: 'VuDuyKhanh',
-      fullName: 'Vũ Duy Khánh'
-    }),
-    Artist({
-      slug: 'HoaVinh',
-      fullName: 'Hoa Vinh'
-    }),
-    Artist({
-      slug: 'NhatPhong',
-      fullName: 'Nhật Phong'
-    }),
-    Artist({
-      slug: 'HoQuangHieu',
-      fullName: 'Hồ Quang Hiếu'
-    }),
-    Artist({
-      slug: 'HoaiLam',
-      fullName: 'Hoài Lâm'
-    }),
-    Artist({
-      slug: 'HuongLy',
-      fullName: 'Hương Ly'
-    }),
-    Artist({
-      slug: 'TrungQuan',
-      fullName: 'Trung Quân Idol'
-    }),
-    Artist({
-      slug: 'KhangViet',
-      fullName: 'Khang Việt'
-    }),
-    Artist({
-      slug: 'KhacViet',
-      fullName: 'Khắc Việt'
-    }),
-    Artist({
-      slug: 'LeBaoBinh',
-      fullName: 'Lê Báo Bình'
-    }),
-    Artist({
-      slug: 'VuongAnhTu',
-      fullName: 'Vương Anh Tú'
-    }),
-    Artist({
-      slug: 'TrangMoon',
-      fullName: 'DJ Trang Moon'
-    }),
   ],
   ['topRemixs']: [
     Product(
@@ -136,20 +64,6 @@ const state = {
       TOP_REMIX_DIR + 'DiCungEm.jpg',
       '33.469.085'
     ),
-    Product(
-      '',
-      '',
-      'https://www.youtube.com/watch?v=NPXFjuSWifw&ab_channel=ACVMusic',
-      TOP_REMIX_DIR + 'SaiLamCuaAnh.jpg',
-      '41.485.802'
-    ),
-    Product(
-      '',
-      '',
-      'https://www.youtube.com/watch?v=bbG-OXqdxn4&ab_channel=LiamLofi',
-      TOP_REMIX_DIR + 'DeVuong.jpg',
-      '13.655.507'
-    ),
   ],
   ['products']: [
     Product(
@@ -172,174 +86,6 @@ const state = {
         'https://www.youtube.com/watch?v=qkPgUgkQE4Y',
         THUMB_DIR + 'dv.jpg',
         '49,073,188'
-    ),
-    Product(
-        'HAI CHỮ ĐÃ TỪNG',
-        'Như Việt',
-        'https://www.youtube.com/watch?v=kxr2ZiQiuS8',
-        THUMB_DIR + 'hcdt.jpg',
-        '45,615,517'
-    ),
-    Product(
-        'CÂU HẸN CÂU THỀ',
-        'Đình Dũng',
-        'https://www.youtube.com/watch?v=QXmn3aw-2dE',
-        THUMB_DIR + 'chct.jpg',
-        '48,603,255'
-    ),
-    Product(
-        'SAI LẦM CỦA ANH',
-        'Đình Dũng',
-        'https://www.youtube.com/watch?v=yn028QF2FPs',
-        'https://i.ytimg.com/vi/yn028QF2FPs/maxresdefault.jpg',
-        '105,293,734'
-    ),
-    Product(
-        'EM ƠI LÊN PHỐ',
-        'Minh Vương M4U',
-        'https://www.youtube.com/watch?v=EHASipZkZnk',
-        'https://i.ytimg.com/vi/EHASipZkZnk/maxresdefault.jpg',
-        '119,326,379'
-    ),
-    Product(
-        'TỪNG YÊU',
-        'PHAN DUY ANH',
-        'https://www.youtube.com/watch?v=YRFSTg9IXNc',
-        'https://i.ytimg.com/vi/yon9M1WSdOk/maxresdefault.jpg',
-        '170,544,656'
-    ),
-    Product(
-        'TÌNH ĐẸP ĐẾN MẤY CŨNG TÀN',
-        'Như Việt',
-        'https://www.youtube.com/watch?v=c5llVZ79WlI',
-        'https://i.ytimg.com/vi/c5llVZ79WlI/maxresdefault.jpg',
-        '56,026,805 '
-    ),
-    Product(
-        'Bông hoa chẳng thuộc về ta',
-        'Như Việt',
-        'https://www.youtube.com/watch?v=JgggA8Jtzyg',
-        THUMB_DIR + 'bhctvt.png',
-        '46,796,902'
-    ),
-    Product(
-        'Không trọn vẹn nữa',
-        'Châu Khải Phong',
-        'https://www.youtube.com/watch?v=JgggA8Jtzyg',
-        THUMB_DIR + 'ktvn.jpg',
-        '11,231,887'
-    ),
-    Product(
-      'Người lạ thoáng qua',
-      'Đinh Tùng Huy',
-      'https://www.youtube.com/watch?v=ZuSHTuOvSGc',
-      THUMB_DIR + 'nltq.jpg',
-      '28,988,762'
-    ),
-    Product(
-        'Hẹn yêu',
-        'Thương Võ',
-        'https://www.youtube.com/watch?v=1qgEfVayp5Q',
-        'https://i.ytimg.com/vi/jhQL1f1agmM/maxresdefault.jpg',
-        '17,963,471'
-    ),
-    Product(
-        'Em nào có tội',
-        'Thương Võ',
-        'https://www.youtube.com/watch?v=7zoUYsKztpE',
-        THUMB_DIR + 'enct.jpg',
-        '18,475,876'
-    ),
-    Product(
-        'Cứ ngỡ là anh',
-        'Đinh Tùng Huy',
-        'https://www.youtube.com/watch?v=CWYPdvUd6mk',
-        'https://i.ytimg.com/vi/CWYPdvUd6mk/maxresdefault.jpg',
-        '42,140,750'
-    ),
-    Product(
-        'Chạy về nơi phía anh',
-        'Khắc Việt',
-        'https://www.youtube.com/watch?v=RaoBKCKIDAI',
-        'https://i.ytimg.com/vi/RaoBKCKIDAI/maxresdefault.jpg',
-        '7,408,892'
-    ),
-    Product(
-        'Anh làm gì sai',
-        'Châu Khải Phong',
-        'https://www.youtube.com/watch?v=1KHmzzUMnTc',
-        THUMB_DIR + 'algs.jpg',
-        '14,525,756'
-    ),
-    Product(
-        'ANH KHÔNG THA THỨ',
-        'Đình Dũng',
-        'https://www.youtube.com/watch?v=mMqEY5j70t0',
-        THUMB_DIR + 'aktt.jpg',
-        '24,851,186'
-    ),
-    Product(
-        'Đi cùng em',
-        'Hà Tròn ft. Lemon Climb',
-        'https://www.youtube.com/watch?v=njbWmwuBiUY',
-        'https://i.ytimg.com/vi/njbWmwuBiUY/maxresdefault.jpg',
-        '14,398,235'
-    ),
-    Product(
-        'Em hứa thế nào',
-        'Như Việt',
-        'https://www.youtube.com/watch?v=7Tg_PteXN4c',
-        THUMB_DIR + 'ehtn.jpg',
-        '2,671,291'
-    ),
-    Product(
-        'Sao ta ngược lối',
-        'Đình Dũng',
-        'https://www.youtube.com/watch?v=F5yUlDpEEGI',
-        THUMB_DIR + 'stnl.jpg',
-        '7,561,283'
-    ),
-    Product(
-        'Nếu có một ngày',
-        'Hương Ly',
-        'https://www.youtube.com/watch?v=PQ0kH_6EvQM',
-        THUMB_DIR + 'ncmn.jpg',
-        '22,054,784'
-    ),
-    Product(
-        'YÊU MỘT NGƯỜI TỔN THƯƠNG',
-        'Nhật Phong',
-        'https://www.youtube.com/watch?v=l_GigiDg3Wc',
-        'https://i.ytimg.com/vi/l_GigiDg3Wc/maxresdefault.jpg',
-        '41,766,974'
-    ),
-    Product(
-        'TRÚC XINH',
-        'Minh Vương M4U ft.Như Việt',
-        'https://www.youtube.com/watch?v=N0ort7baD0o',
-        'https://i.ytimg.com/vi/N0ort7baD0o/maxresdefault.jpg',
-        '20,578,970'
-    ),
-    Product(
-        'Sợ ta mất nhau',
-        'Châu Khải Phong',
-        'https://www.youtube.com/watch?v=RGf0jUKUj7k',
-        'https://i.ytimg.com/vi/iJWE34DwTEE/maxresdefault.jpg',
-        '5,973,229'
-    ),
-    Product(
-        'Yêu em thật đấy',
-        'Đinh Tùng Huy',
-        'https://www.youtube.com/watch?v=dl5z_oQlETc',
-        'https://i.ytimg.com/vi/dl5z_oQlETc/maxresdefault.jpg',
-        '7,290,115'
-    ),
-    Product(
-        'Đừng hẹn kiếp sau',
-        'Đình Dũng',
-        'https://www.youtube.com/watch?v=1qgEfVayp5Q',
-        THUMB_DIR + 'dhks.jpg',
-        '17,963,538'
     ),
   ],
 };
@@ -600,7 +346,7 @@ function goToPage(page, limit, root, renderCallback) {
 }
 
 function initCharts() {
-  const TOTAL_CHART_IMG = 18;
+  const TOTAL_CHART_IMG = 3;
   for (let i = TOTAL_CHART_IMG; i > 0; i--) {
     state['charts'].push(Chart(CHART_DIR + `IMG_${i}.JPG`));
   }
